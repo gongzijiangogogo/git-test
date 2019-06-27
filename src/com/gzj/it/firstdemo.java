71ef3701b893808ecacbb1527af5df061c73c743
gzj <beizhiwu123@163.com>
2019-06-27 20:56:09 +0800

    hhh

---
+++ src/com/gzj/it/firstdemo.java
@@ -0,0 +1,11 @@
+package com.gzj.it;
+
+public class firstdemo {
+    public static void main(String[] args) {
+        System.out.println("hhh");
+        System.out.println("hhh");
+        System.out.println("hhh");
+        System.out.println("hhh");
+        System.out.println("hhh");
+    }
+}
